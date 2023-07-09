145b8021af61d2f0d4ed438273b103c6d7809c2f
Brian Kiiza <bkiiza92@gmail.com>
2023-07-09 18:23:21 -0400

    Update

---
+++ send-email.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    $to = "bkiiza92@gmail.com";
+    $subject = "Message from Portfolio Website";
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    mail($to, $subject, $message, $headers);
+    echo "Email sent.";
+}
+?>
